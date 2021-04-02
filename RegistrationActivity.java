738e956bdbbb44ab1d05e1760808cdb940295de4
19IT116 <68849770+19IT116@users.noreply.github.com>
2021-04-02 09:23:04 +0530

    Add files via upload

--- RegistrationActivity.java
+++ RegistrationActivity.java
@@ -1,31 +1,36 @@
 package com.example.settleup;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity  {
+
+    GoogleSignInClient mGoogleSignInClient;
 
     private EditText mEmail;
     private EditText mPass;
-    private Button btnReg;
-    private TextView mSignin;
 
     private ProgressDialog mDialog;
 
@@ -33,6 +38,10 @@ public class RegistrationActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
+    TextView textView;
+    private static final int RC_SIGN_IN = 100;
 
 
     @Override
@@ -40,66 +49,164 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+
         mAuth=FirebaseAuth.getInstance();
 
         mDialog=new ProgressDialog(this);
 
         registration();
+
+        GoogleSignInOptions gso =  new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+//        googleApiClient=new GoogleApiClient.Builder(this)
+//                .enableAutoManage(this,this)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+//                .build();
+
+
+
+        signInButton=(SignInButton)findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+
     }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+
+                Toast.makeText(this, "User email"+personEmail, Toast.LENGTH_SHORT).show();
+            }
+
+            startActivity(new Intent(RegistrationActivity.this , MainActivity.class));
+
+            // Signed in successfully, show authenticated UI.
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+
+            Log.d("Message" , e.toString());
+        }
+    }
+
+
+//    @Override
+//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode==RC_SIGN_IN){
+//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+//            handleSignInResult(result);
+//        }
+//    }
+//
+//    private void handleSignInResult(GoogleSignInResult result) {
+//
+//        if(result.isSuccess()){
+//            gotoProfile();
+//        }else{
+//            Toast.makeText(getApplicationContext(),"Sign in cancel",Toast.LENGTH_LONG).show();
+//        }
+//    }
+    private void gotoProfile(){
+        Intent intent=new Intent(RegistrationActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
 
     private void registration(){
 
         mEmail=findViewById(R.id.email_reg);
         mPass=findViewById(R.id.password_reg);
-        btnReg=findViewById(R.id.btn_reg);
-        mSignin=findViewById(R.id.signin_here);
+        Button btnReg = findViewById(R.id.btn_reg);
+        TextView mSignin = findViewById(R.id.signin_here);
 
-        btnReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnReg.setOnClickListener(v -> {
 
-                String email=mEmail.getText().toString().trim();
-                String pass=mPass.getText().toString().trim();
+            String email=mEmail.getText().toString().trim();
+            String pass=mPass.getText().toString().trim();
+            new AuthUI.IdpConfig.GoogleBuilder().build();
 
-                if(TextUtils.isEmpty(email)){
-                    mEmail.setError("Email Required..");
-                    return;
-                }
-                if(TextUtils.isEmpty(pass)){
-                    mPass.setError("Password Required..");
-                }
-
-                mDialog.setMessage("Processing..");
-                mDialog.show();
-
-                mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if(task.isSuccessful()){
-
-                            mDialog.dismiss();
-
-                            Toast.makeText(getApplicationContext(),"Registration Complete",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
-                        }else{
-                            mDialog.dismiss();
-                            Toast.makeText(getApplicationContext(),"Registration Field..",Toast.LENGTH_SHORT).show();
-
-                        }
-
-                    }
-                });
-
+            if(TextUtils.isEmpty(email)){
+                mEmail.setError("Email Required..");
+                return;
             }
+            if(TextUtils.isEmpty(pass)){
+                mPass.setError("Password Required..");
+            }
+
+            mDialog.setMessage("Processing..");
+            mDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(task -> {
+
+                if(task.isSuccessful()){
+
+                    mDialog.dismiss();
+
+                    Toast.makeText(getApplicationContext(),"Registration Complete",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                }else{
+                    mDialog.dismiss();
+                    Toast.makeText(getApplicationContext(),"Registration Field..",Toast.LENGTH_SHORT).show();
+
+                }
+
+            });
+
         });
 
-        mSignin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            }
-        });
+        mSignin.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),MainActivity.class)));
 
     }
 }
